9edc8e1914ee91fa5c2655f36d3009c75168f97f
sezgink <sezgink@users.noreply.github.com>
2023-05-29 00:59:10 +0300

    Current docker file and requirements

---
+++ dags/scrape_process.py
@@ -0,0 +1,25 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+
+crawl_process_dag = DAG(
+    dag_id="crawl_process_dag",
+    # start_date=datetime.datetime(2021, 1, 1),
+    # schedule="@daily",
+    start_date=datetime.datetime.now(),
+    schedule="*/2 * * * *",
+    dagrun_timeout=datetime.timedelta(seconds=5)
+)
+scrape = BashOperator(
+    task_id='scrape',
+    bash_command='echo "Scraping now" '
+)
+
+process = BashOperator(
+    task_id='process',
+    bash_command='echo "Processing now"'
+)
+
+scrape >> process
