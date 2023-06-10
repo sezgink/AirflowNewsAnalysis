4ef321b2960e1bc2aa00d3d505fc9900e94382af
sezgink <sezgink@users.noreply.github.com>
2023-06-10 15:19:56 +0300

    Scrape process DAG update

--- dags/scrape_process.py
+++ dags/scrape_process.py
@@ -4,34 +4,36 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 
-# default_args = {
-#     'owner': 'Sertan Kutlu',
-#     'depends_on_past': False,
-#     'start_date': datetime(2015, 6, 1),
-#     'email': ['airflow@example.com'],
-#     'email_on_failure': False,
-#     'email_on_retry': False,
-#     'retries': 1,
-#     'retry_delay': timedelta(minutes=5),
-#     # 'queue': 'bash_queue',
-#     # 'pool': 'backfill',
-#     # 'priority_weight': 10,
-#     # 'end_date': datetime(2016, 1, 1),
-# }
+default_args = {
+    'owner': 'Sertan Kutlu',
+    'depends_on_past': False,
+    'start_date': datetime.datetime(2015, 6, 1),
+    'email': ['sezgink@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
 
 crawl_process_dag = DAG(
     dag_id="crawl_process_dag",
+    default_args=default_args,
     # start_date=datetime.datetime(2021, 1, 1),
     # schedule="@daily",
-    start_date=datetime.datetime.now()-datetime.timedelta(days=1),
+    # start_date=datetime.datetime.now()-datetime.timedelta(days=1),
     schedule="*/2 * * * *",
-    dagrun_timeout=datetime.timedelta(seconds=5)
+    dagrun_timeout=datetime.timedelta(minutes=2)
 )
 scrape = BashOperator(
     task_id="scrape",
     # bash_command='echo "Scraping now" ',
-    # bash_command='ls',
-    bash_command='python /opt/airflow/TwitterScraper/src/twitterdatacollector.py single elonmusk',
+    # bash_command='pwd',
+    execution_timeout=datetime.timedelta(minutes=2),
+    bash_command='python /opt/airflow/TwitterScraper/src/twitterdatacollector.py single elonmusk /opt/airflow/data/fetchedTweets.csv',
     dag=crawl_process_dag
 )
 
